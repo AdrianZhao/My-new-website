d1883cbf14f43b678e9a2094eba36854ac63aaeb
Adrian Zhao <achnnal@gmail.com>
2023-03-02 10:39:01 -0600

    animation

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,3 +3,12 @@
   JavaScript Basics
   Yuhan Zhao
 */
+
+const anim = document.querySelector('.text .click-btn');
+            
+anim.addEventListener('click', function() {             
+  document.querySelector('.red').classList.toggle('red-expand');
+  document.querySelector('.green').classList.toggle('green-expand');
+  document.querySelector('.yellow').classList.toggle('yellow-expand');
+  document.querySelector('.blue').classList.toggle('blue-expand');
+})
